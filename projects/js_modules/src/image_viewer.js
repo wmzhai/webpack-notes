bfa04ae0d5f869a121586e5651cd3e35e41772cf
Weiming Zhai <wmzhai@gmail.com>
2017-02-25 20:37:32 +0800

    webpack small image

--- projects/js_modules/src/image_viewer.js
+++ projects/js_modules/src/image_viewer.js
@@ -1,6 +1,12 @@
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = "http://lorempixel.com/400/400";
 
+const image = document.createElement('img');
+image.src = small;
 document.body.appendChild(image);
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
+document.body.appendChild(bigImage);
