103ff45e3f3d3ce537d137765ec1004ca8744cee
Weiming Zhai <wmzhai@gmail.com>
2017-02-26 08:48:23 +0800

    add chunkhash to output js filename

--- projects/proj2/webpack.config.js
+++ projects/proj2/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -39,7 +39,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vender'
+      names: ['vender','manifest']
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
